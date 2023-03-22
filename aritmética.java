93fa732c7d1473ac344deb034799a75309d09e0d
Francis Nicole Baños Flores <nikitasabha@gmail.com>
2023-03-22 17:37:22 -0400

    primer commit

--- aritmética.java
+++ aritmética.java
@@ -1,10 +1,4 @@
-
-class Suma {
- 
-   public static void main( String[] args ) {
-      int a = 15, b = 10, c = 0;
-      c = a + b;
-      System.out.println( "La suma de a y b es: " + c );
-   }
- 
-}
+function calcularSuma(num1, num2) {
+    return num1 + num2;
+  }
+  console.log(calcularResta(22,77))
