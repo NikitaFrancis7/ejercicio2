20113be8ebc67fd43c3678680f67acaf3db4f091
jorge ledezma <jledezmaze@yahoo.com>
2023-03-22 20:11:59 -0400

    funcion resta

--- modulo.js
+++ modulo.js
@@ -1,4 +1,13 @@
 function calcularModulo(dividendo, divisor) {
     return dividendo % divisor;
   }
-  console.log(calcularModulo(10,3))
+  console.log(calcularModulo(10,3)
+
+  )
+
+function resta(num1, num2){
+	var res = num1 - num2;
+	return res;
+}
+
+console.log(resta(100,2));
